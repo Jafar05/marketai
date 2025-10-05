2aba56fea9579258ff1d813d88d04501820d2745
jafar <jafar_magomedov0594@mail.ru>
2025-10-05 14:05:44 +0300

    Добавил config

--- auth/cmd/auth/main.go
+++ auth/cmd/auth/main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"marketai/auth/internal/config"
 	"marketai/auth/internal/ports"
 
 	"go.uber.org/fx"
 )
 
 func main() {
-	config.LoadConfig()
+	// config.LoadConfig()
 	fx.New(ports.App()).Run()
 }
