51f019e4a43695fac56e32f4d4fa871dffc9ed9b
jafar <jafar_magomedov0594@mail.ru>
2025-10-05 19:44:47 +0300

    поправил название колонки

--- auth/internal/adapters/postgres/query.go
+++ auth/internal/adapters/postgres/query.go
@@ -10,7 +10,7 @@ const (
 
 	createUser = `
 		INSERT INTO users
-			(id, email, password_hash, phoneNumber, role, created_at, updated_at)
+			(id, email, password_hash, phone_number, role, created_at, updated_at)
         VALUES (gen_random_uuid(), $1, $2, $3, $4, $5, $6)
 		RETURNING id`
 )
